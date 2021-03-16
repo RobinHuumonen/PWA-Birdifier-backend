91b7f1bd0e67cfe726be83641801aa40ce509f70
RobinHuumonen <robin.huumonen@hotmail.com>
2021-03-16 12:59:24 +0000

    uibuild

--- app.js
+++ app.js
@@ -10,18 +10,11 @@ app.use(express.json());
 
 app.use(middleware.requestLogger);
 
-//app.use(router);
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-/* if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
-} */
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
